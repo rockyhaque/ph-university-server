cf0a2135eae6cd80506cc6f6344af69f831a668f
Rocky Haque <rockyhaque99@gmail.com>
2024-12-18 18:28:41 +0600

    academic-semester created

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -5,9 +5,7 @@ import catchAsync from '../../utils/catchAsync';
 
 const createStudent = catchAsync(async (req, res) => {
   const { password, student: studentData } = req.body;
-
-
-
+  
   const result = await UserServices.createStudentIntoDB(password, studentData);
 
   sendResponse(res, {
