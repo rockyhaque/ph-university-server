400dc74082dc0eca40eaa15bb70b30974c5016a3
Rocky Haque <rockyhaque99@gmail.com>
2025-01-08 12:53:12 +0600

    fix: correct import path for CourseServices in course.controller.ts

--- src/app/modules/Course/course.controller.ts
+++ src/app/modules/Course/course.controller.ts
@@ -1,7 +1,8 @@
 import httpStatus from 'http-status';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
-import { CourseServices } from './courseService';
+import { CourseServices } from './course.service';
+
 
 const createCourse = catchAsync(async (req, res) => {
   const result = await CourseServices.createCourseIntoDB(req.body);
